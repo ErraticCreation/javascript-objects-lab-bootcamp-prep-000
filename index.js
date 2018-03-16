32332e546167edf7f0540199d63b8e1ce7bb9f58
Tarah Nixon <tarah.sage.nixon@gmail.com>
2018-03-16 22:48:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ var updateObjectWithKeyAndValue = function(object, key, value) {
   var newObject = Object.assign({ [key]: value }, object);
   return newObject;
 }
+
+var destructivelyUpdateObjectWithKeyAndValue = function(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
